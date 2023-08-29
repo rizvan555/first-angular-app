d8d9a26f58f9cf64d6f3276e53a786e936e7fc79
Rizvan <rizvan@MacBook-Air-2.fritz.box>
2023-08-29 20:21:37 +0200

    connect tempalate Typescript with class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Hallo</h1>
+    <h3>{{ name }}</h3>
+    <h4>I am {{ myAge }} years old.</h4>
+    <h5 [innerHTML]="contain"></h5>
+    <input type="text" value="{{ inputValue }}" />`,
+
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  name: string = 'Rizvan Karimov';
   contain: string = 'I am learning Angular ';
   myAge: string | number = 39;
+  inputValue: string = 'Enter your Name';
   // string
   // number
   // boolean
